806b47a1808f9d7e8fd7a6821977cc2113b54f93
Siyuan Zheng <raymondzsy@outlook.com>
2016-11-27 20:32:48 -0500

    Raymond: get the search algorithm and communication thru backend and frontend working

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -2,6 +2,9 @@
 
 var dashboard = {};
 
+
+//append search content at the div with id 'searchDisplayPanel'
+
 dashboard.init = function() {
     $("#post-form").submit(function(event) {
         event.preventDefault();
@@ -23,8 +26,104 @@ dashboard.init = function() {
     $("#pay").on("input", function() {
         $("#pay-amount").text("$ " + $(this).val() + " / hour");
     })
+
+    //onclick handler for the search button...
+    //backend and frontend connection works now..
+    $('#searchSubmit').click(function(){
+      var entered = $('#searchContent').val();
+      var type = searchContentType(entered);
+      if(type === -1){ //invalid search, thus do not send http request
+          window.alert('Please modify your search field and try again');
+      }else if(type === 0){ //tutor name
+          //window.alert('0'); //type check works
+          $('#searchDisplayPanel').empty();
+          $.get('/search?tutorName=' + entered, function(response){
+              //window.alert(response); works  front -> back ->front(response) works
+              if(response.length !== 0){
+                console.log(response);
+              }else{
+                window.alert('No search tutor name');
+              }
+          })
+      }else if(type === 3){ //3-char course code
+          //window.alert('3'); //type check works
+          $('#searchDisplayPanel').empty();
+          $.get('/search?shortCourseCode=' + entered, function(response){
+              // window.alert(response); works  front -> back ->front(response) works
+              if(response.length !== 0){
+                console.log(response);
+              }else{
+                window.alert('No such course code');
+              }
+          })
+      }else if(type === 6){ //6-char course code
+          //window.alert('6'); //type check works
+          $('#searchDisplayPanel').empty();
+          $.get('/search?completeCourseCode=' + entered.toUpperCase(), function(response){
+              //window.alert(response); works  front -> back ->front(response) works
+              if(response.length !== 0){
+                console.log(response);
+              }else{
+                window.alert('No such course code');
+              }
+          })
+      }
+    });
 }
 
 $(document).ready(function() {
     dashboard.init();
 });
+
+
+//works
+//helper function for decides what type of content is being searched..
+//-1 means non-valie while
+//0 means pure letters without digit
+//3 means course code with form ###
+//6 means course code with form LLL###
+//valid course code(LLL### or ###) where L stands for English letter
+function searchContentType(content){
+  var length = content.length;
+  if(length === 0){
+    return -1;
+  }else{ //non-empty
+    if(checkNum(content) === 0){ //no digit, should represent a tutor name
+      return 0;
+    }else if(checkNum(content) === 1){ //has at least one digit
+        if(length === 3){
+            if(!isNaN(content.charAt(0)) && !isNaN(content.charAt(1)) && !isNaN(content.charAt(2))){
+                return 3;
+            }else{ //incorrect search content
+                return -1;
+            }
+        }else if(length === 6){
+            if(isNaN(content.charAt(0)) && isNaN(content.charAt(1)) && isNaN(content.charAt(2))
+            && !isNaN(content.charAt(3)) && !isNaN(content.charAt(4)) && !isNaN(content.charAt(5))){
+              return 6;
+            }else{
+              return -1;
+            }
+        }else{
+          return -1;
+        }
+    }
+  }
+}
+
+
+//works
+//helper funtion for checking whether a string contains a digit
+function checkNum(string){
+  var length = string.length;
+  if(length == 0){
+    return -1; //invalid
+  }else{ //user has entered content..
+    for(let i = 0; i < length ; i++){
+      if(!isNaN(string.charAt(i))){
+        return 1; //string has digit
+      }
+    }
+  }
+  return 0; //string does not have digit
+}
--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,33 @@
 'use strict';
 
-var User = require("../models/user"); // Can be removed if not used
+var User = require("../models/user");
 var Course = require("../models/course");
-
+//data from courses_seed.json and from users_seed.json have been
+//loaded to the corresponding mongo database via command in terminal..
 
 // TODO Raymond: add any functions they you need.
-exports.getSearch = function(req, res) {};
+//build the response here..
+exports.getSearch = function(req,res) {
+    var tutorName = req.query.tutorName;
+    var shortCourseCode = req.query.shortCourseCode;
+    var completeCourseCode = req.query.completeCourseCode;
+    //the below if statement has covered all the possibilities
+    if(tutorName !== undefined){
+        //res.send('tutorName success to backend'); //front -> back -> front test
+        User.find({type:'tutor',username:tutorName},function(err,user){
+          if(err) throw err;
+          res.send(user);
+        })
+    }else if(shortCourseCode !== undefined){
+        Course.find({code: {$regex:shortCourseCode}},function(err,courses){
+          if(err) throw err;
+          res.send(courses);
+        })
+    }else if(completeCourseCode !== undefined){
+        //res.send('completeCourseCode Success to backend'); //front -> back -> front test
+        Course.find({code:completeCourseCode},function(err,course){
+          if(err) throw err;
+          res.send(course);
+        })
+    }
+};
